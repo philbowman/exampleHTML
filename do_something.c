73488b4a968f20f83a238ce7423dd5f1a140c18a
philbowman <47841859+philbowman@users.noreply.github.com>
2022-10-09 14:45:30 +0300

    Added bootstrap

---
+++ do_something.c
@@ -0,0 +1,21 @@
+for (int i = 0 i < 10; i ++)
+{
+    int foo = 10;
+    do_something(foo);
+    bool foo = false;
+    int foo = 0;
+    string foo = "";
+    long foo \0 null
+}
+
+int main (int argc, char* argv[])
+{
+    int doubled = double(10, 2);
+}
+
+int multiplication (int x, int y)
+{
+    return x * y;
+}
+
+
